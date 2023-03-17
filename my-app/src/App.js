bdf885af5442a0909af26755006170335850fa98
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-03-17 10:15:32 +0200

    add nav bar and start page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,14 @@
+import { Carousel } from "./componets/Carousel";
+import { NavBar } from "./componets/NavBar";
+import { TopBar } from "./componets/TopBar";
 
 function App() {
   return (
     <div className="App">
-     
+      <TopBar />
+      <NavBar />
+      <Carousel />
+
     </div>
   );
 }
