d9b382a5ea6fd3e23c52765e1d3616f671fd2f03
Martin <88441320+MartoIT@users.noreply.github.com>
2023-03-20 23:48:03 +0200

    Update footer vol 2

--- my-app/src/componets/Footer/Footer.js
+++ my-app/src/componets/Footer/Footer.js
@@ -47,20 +47,8 @@ export const Footer = () => {
             </div>
         </div>
     </div>
-    <div className="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style={{borderColor: "rgba(256, 256, 256, .1) !important" }}>
-        <div className="row">
-            <div className="col-lg-6 text-center text-md-left mb-3 mb-md-0">
-                <p className="m-0 text-white-50">Copyright &copy; <a href="#/">Domain</a>. All Rights Reserved.</p>
-                
-            </div>
-            <div className="col-lg-6 text-center text-md-right">
-                <p className="m-0 text-white-50">Designed by <a href="https://htmlcodex.com">HTML Codex</a>
-                </p>
-            </div>
-        </div>
-    </div>
-    <a href="#/" className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="fa fa-angle-double-up"></i></a>
-
+   
+  
     </>
     );
 };
