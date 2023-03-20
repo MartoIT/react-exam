a6e4bfb9609da985b6249f2cf8ff926cec7e660d
Martin <88441320+MartoIT@users.noreply.github.com>
2023-03-20 23:27:29 +0200

    Up date registration form

--- my-app/src/componets/Registration/Registration.js
+++ my-app/src/componets/Registration/Registration.js
@@ -1,48 +1,58 @@
 export const Registration = () => {
     return (
-        <div className="container-fluid bg-registration py-5" style={{margin: '90px 0'}}>
-        <div className="container py-5">
-            <div className="row align-items-center">
-                <div className="col-lg-7 mb-5 mb-lg-0">
-                    <div className="mb-4">
-                        <h6 className="text-primary text-uppercase" style={{letterSpacing: '5px'}}>Mega Offer</h6>
-                        <h1 className="text-white"><span className="text-primary">30% OFF</span> For Honeymoon</h1>
-                    </div>
-                    <p className="text-white">Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                        ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
-                        dolor</p>
-                    <ul className="list-inline text-white m-0">
-                        <li className="py-2"><i className="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
-                        <li className="py-2"><i className="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
-                        <li className="py-2"><i className="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
-                    </ul>
-                </div>
-                <div className="col-lg-5">
-                    <div className="card border-0">
-                        <div className="card-header bg-primary text-center p-4">
-                            <h1 className="text-white m-0">Sign Up Now</h1>
+        <div className="container-fluid bg-registration py-5" style={{ margin: '90px 0' }}>
+            <div className="container py-5">
+                <div className="row align-items-center">
+                    <div className="col-lg-7 mb-5 mb-lg-0">
+                        <div className="mb-4">
+                            <h6 className="text-primary text-uppercase" style={{ letterSpacing: '5px' }}>Mega Offer</h6>
+                            <h1 className="text-white"><span className="text-primary">30% OFF</span> For Honeymoon</h1>
                         </div>
-                        <div className="card-body rounded-bottom bg-white p-5">
-                            <form>
-                                <div className="form-group">
-                                    <input type="text" className="form-control p-4" placeholder="Your name" required="required" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="email" className="form-control p-4" placeholder="Your email" required="required" />
-                                </div>
-                                <div className="form-group">
-                                <input type="password" className="form-control p-4" placeholder="Password" required="required" />
-                                </div>
-                                <div>
-                                    <button className="btn btn-primary btn-block py-3" type="submit">Sign Up Now</button>
-                                </div>
-                                <p style={{margin: "10px"}}>Already have an account? <a href="/Login"> Log in</a></p>
-                            </form>
+                        <p className="text-white">Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
+                            ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
+                            dolor</p>
+                        <ul className="list-inline text-white m-0">
+                            <li className="py-2"><i className="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
+                            <li className="py-2"><i className="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
+                            <li className="py-2"><i className="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
+                        </ul>
+                    </div>
+                    <div className="col-lg-5">
+                        <div className="card border-0">
+                            <div className="card-header bg-primary text-center p-4">
+                                <h1 className="text-white m-0">Sign Up Now</h1>
+                            </div>
+                            <div className="card-body rounded-bottom bg-white p-5">
+                                <form>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control p-4" placeholder="Name" required="required" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="age" className="form-control p-4" placeholder="Age" required="required" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control p-4" src="sdasda" alt="Submit" width="100" height="100" placeholder="Avatar link here..." />
+                                        
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="email" className="form-control p-4" placeholder="Email" required="required" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="password" className="form-control p-4" placeholder="Password" required="required" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="password" className="form-control p-4" placeholder="Repeat pass" required="required" />
+                                    </div>
+                                    <div>
+                                        <button className="btn btn-primary btn-block py-3" type="submit">Sign Up Now</button>
+                                    </div>
+                                    <p style={{ margin: "10px" }}>Already have an account? <a href="/Login"> Log in</a></p>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
