4f9aad214befd50d3ed28a9f7ebbe8098f2a69d2
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-03-20 16:59:56 +0200

    update nav bar wen clikc on a logo to relocation to home page

--- my-app/src/componets/NavBar/NavBar.js
+++ my-app/src/componets/NavBar/NavBar.js
@@ -3,7 +3,7 @@ export const NavBar = () => {
         <div className="container-fluid position-relative nav-bar p-0">
             <div className="container-lg position-relative p-0 px-lg-3" style={{ zIndex: "9" }} >
                 <nav className="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
-                    <a href="#/" className="navbar-brand">
+                    <a href="/" className="navbar-brand">
                         <h1 className="m-0 text-primary"><span className="text-dark">TRAVEL</span>ER</h1>
                     </a>
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
