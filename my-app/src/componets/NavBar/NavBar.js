5f4832d9c195db54cc7416b56ded188ac71cc741
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-03-21 13:54:40 +0200

    update nav bar remove all href

--- my-app/src/componets/NavBar/NavBar.js
+++ my-app/src/componets/NavBar/NavBar.js
@@ -5,26 +5,26 @@ export const NavBar = () => {
         <div className="container-fluid position-relative nav-bar p-0">
             <div className="container-lg position-relative p-0 px-lg-3" style={{ zIndex: "9" }} >
                 <nav className="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
-                    <a href="/" className="navbar-brand">
+                   <Link to={"/"} className="navbar-brand">
                         <h1 className="m-0 text-primary"><span className="text-dark">TRAVEL</span>ER</h1>
-                    </a>
+                    </Link>
                     <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
                         <div className="navbar-nav ml-auto py-0">
-                            <a href="/" className="nav-item nav-link active">Home</a>
+                            <Link to={"/"} className="nav-item nav-link active">Home</Link>
                             <div class="nav-item dropdown">
                                 <a href="#/" className="nav-link dropdown-toggle" data-toggle="dropdown">Services Pages</a>
                                 <div class="dropdown-menu border-0 rounded-0 m-0">
-                                    <a href="/services" class="dropdown-item">Services</a>
-                                    <a href="/packages" class="dropdown-item">Tour Packages</a>
-                                    <a href="/destination" class="dropdown-item">Destination</a>
+                                   <Link to={"/services"} class="dropdown-item">Services</Link> 
+                                   <Link to={"/packages"} class="dropdown-item">Packages</Link>
+                                   <Link to={"/destination"} class="dropdown-item">Destination</Link> 
                                 </div>
                             </div>
-                            <a href="/registration" className="nav-item nav-link">Registration</a>
-                            <a href="/login" className="nav-item nav-link">login</a>
-                            <a href="/registration" className="nav-item nav-link">logout</a>
+                           <Link to={"/registration"} className="nav-item nav-link">Registration</Link> 
+                           <Link to={"/login"} className="nav-item nav-link">login</Link>
+                           <Link to={"/registration"} className="nav-item nav-link">logout</Link> 
                             <div class="nav-item dropdown">
                                 <a href="#/" className="nav-link dropdown-toggle" data-toggle="dropdown">About</a>
                                 <div class="dropdown-menu border-0 rounded-0 m-0">
