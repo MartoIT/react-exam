b922ac27cc7f97480e66bf31713789bd6b432b0a
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-03-30 10:19:52 +0300

    update reviews to show in line the app is on a final line

--- my-app/src/componets/Testimonial/Testimonial.js
+++ my-app/src/componets/Testimonial/Testimonial.js
@@ -6,20 +6,17 @@ import { reviewServicessFactory } from '../../services/reviewServices';
 import { StarRating } from "../../utils/StarRating";
 import { Review } from "./review";
 
-export const Testimonial = ({
-   
-    onReviewPost
-}) => {
-    const { userId, token, isAuthenticated, username, imageUrl } = useContext(AuthContext);
+export const Testimonial = () => {
+    const { token, username, imageUrl } = useContext(AuthContext);
     const [review, setReview] = useState([]);
     const reviewServices = reviewServicessFactory();
     useEffect(() => {
         reviewServices.getAll()
-          .then(result => {
-            setReview(result)
-          })
-      }, []);
-    
+            .then(result => {
+                setReview(result)
+            })
+    }, []);
+
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -30,60 +27,49 @@ export const Testimonial = ({
             stars,
             imageUrl,
             username
-        }
+        };
 
         const newReview = await reviewServices.addReview(data, token);
-        //onReviewPost(data, token)
         console.log(newReview)
         reviewServices.getAll()
-        .then(result => {
-          setReview(result)
-        })
-  
-        
-        
+            .then(result => {
+                setReview(result)
+            });
+
     }
     return (
+
         <div className="container-fluid py-5">
-             
-
-
-            <div className="container pt-5 pb-3" >
-                <div  >
-                {review.map(r => <Review key={Math.random()} {...r} />)}
-
+            <div className="container py-5">
+                <div className="text-center mb-3 pb-3">
+                    <h6 className="text-primary text-uppercase" style={{ letterSpacing: "5px" }}>Testimonial</h6>
+                    <h1>What Say Our Clients</h1>
                 </div>
-            
+
+                <div className="row">
+                    {review.map(r => <Review key={Math.random()} {...r} />)}
+                </div>
                 <div className="col-lg-5">
                     <div className="card border-0">
                         <div className="card-header bg-primary text-center p-4" >
                             <h1 className="text-white m-0">Love what we do</h1>
                             <h2 className=" m-0">Leave us a review</h2>
-                            
                         </div>
                         <div className="card-body rounded-bottom bg-white p-5" >
                             <form onSubmit={onSubmit}>
                                 {<StarRating />}
-                                
                                 <div className="form-group">
                                     <textarea className="form-control p-4" name="review" placeholder="leave a review" rows="4" cols="40" />
-
                                 </div>
                                 <div>
                                     <button className="btn btn-primary btn-block py-3" type="submit">Send</button>
                                 </div>
-
-                               
-
                             </form>
-
                         </div>
                     </div>
                 </div>
-                
             </div>
-            
-
         </div>
+
     );
 };
--- my-app/src/componets/Testimonial/review.js
+++ my-app/src/componets/Testimonial/review.js
@@ -7,26 +7,18 @@ export const Review = ({
 
     const current = Number(stars)
     return (
-
-       
-            <div className="row" >
-                <div className="col-lg-3 col-md-4 col-sm-6 pb-2">
-                    <div >
-                        <img src={imageUrl} style={{ width: "100px", height: "100px" }} alt="flu" />
-
-                        <div className="testimonial-text bg-white p-4 mt-n5">
-                            <p className="mt-5">{review}</p>
-                            <div className="icons align-items-center">
-                                {[...Array(current)].map(() => (<i key={Math.random()} className="fa fa-star text-warning"></i>))}
-                            </div>
-                            <h5 className="text-truncate">Client Name</h5>
-                            <span>{username}</span>
-                        </div>
-
-                    </div>
+        <div className="col-lg-3 col-md-4 col-sm-6 pb-2">
+        <div className="team-img position-relative overflow-hidden">
+            <img src={imageUrl} className="img-fluid w-100" alt="flu" />
+            <div className="testimonial-text bg-white p-4 mt-n5">
+                <p className="mt-5">{review}</p>
+                <div className="icons align-items-center">
+                    {[...Array(current)].map(() => (<i key={Math.random()} className="fa fa-star text-warning"></i>))}
                 </div>
-
+                <h5 className="text-truncate">Client Name</h5>
+                <span>{username}</span>
             </div>
-       
+        </div>
+        </div>
     );
 };
