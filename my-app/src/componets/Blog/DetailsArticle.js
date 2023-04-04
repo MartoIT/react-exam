60efa02214a3cb2ce8a64add3f3e695590a0ce6b
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-04-04 09:27:28 +0300

    add like ikon and pen icon on a conments

--- my-app/src/componets/Blog/DetailsArticle.js
+++ my-app/src/componets/Blog/DetailsArticle.js
@@ -1,5 +1,8 @@
+
+
 import { useEffect, useState, useContext } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
+
 
 
 
@@ -127,46 +130,50 @@ export const DetailsArticle = ({ onDeleteClick, onComentAdd }) => {
                 <div >
                     <h2>Comments:</h2>
                     <div className="container mt-5">
-                        
+
 
                         <div className="row  d-flex justify-content-center">
 
                             <div className="col-md-8">
 
-                            {comment && Object.values(comment).map(x => (
-                            
-                                
-                        
-                                <div key={x._id}  className="card p-3">
+                                {comment && Object.values(comment).map(x => (
 
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        
-                                        <div className="user d-flex flex-row align-items-center">
 
-                                            <img src={x.imageUrl} width="80" className="user-img rounded-circle mr-2" />
-                                            <span><small className="font-weight-bold text-primary">{x.username}</small> <small className="font-weight-bold">{x.comment}</small></span>
 
-                                        </div>
-                                    </div>
+                                    <div key={x._id} className="card p-3">
 
-                                    <div className="action d-flex justify-content-between mt-2 align-items-center">
+                                        <div className="d-flex justify-content-between align-items-center">
 
-                                      
+                                            <div className="user d-flex flex-row align-items-center">
 
-                                        <div className="icons align-items-center">
+                                                <img src={x.imageUrl} width="80" className="user-img rounded-circle mr-2" alt='some' />
+                                                <span><small className="font-weight-bold text-primary">{x.username}</small> <small className="font-weight-bold">{x.comment}</small></span>
 
-                                            <i className="fa fa-star text-warning"></i>
-                                            <i className="fa fa-star text-warning"></i>
-                                            <i className="fa fa-star text-warning"></i>
-                                            <i className="fa fa-star text-warning"></i>
-                                            <i className="fa fa-star text-warning"></i>
-                                            <i className="fa fa-check-circle-o check-icon"></i>
-
+                                            </div>
                                         </div>
 
-                                    </div>
+                                        <div className="action d-flex justify-content-between mt-2 align-items-center">
 
-                                </div>
+
+
+                                            <div className="">
+                                            <i class="fas fa-pen"></i>
+                                            </div>
+                                            <div>
+                                            <i class="fas fa-thumbs-up"></i>
+
+                                            </div>
+                                            <div>
+                                            <i class="fa fa-trash-o" style={{fontSize: "36px"}}></i>
+                                            </div>
+                                               
+                                               
+                                                {/* <i className="fa fa-check-circle-o check-icon"></i> */}
+                                              
+
+                                        </div>
+
+                                    </div>
                                 ))}
 
 
@@ -175,7 +182,7 @@ export const DetailsArticle = ({ onDeleteClick, onComentAdd }) => {
                         </div>
 
                     </div>
-                    
+
                     {!Object.values(comment).length && (
                         <div>
                             <p >0 comments yet</p>
